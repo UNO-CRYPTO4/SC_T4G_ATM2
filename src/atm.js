1f730467f98da37656bdbca20e1e53ab854d3114
ElpeAJ <elpediaarthurjunior@gmail.com>
2026-07-27 03:45:15 +0000

    Adding JS file with steps as comments

---
+++ src/atm.js
@@ -0,0 +1,26 @@
+// VISUAL ATM SYSTEM
+// INSTRUCTIONS: Fill in the missing logic pieces inside wher there are Task comments!
+
+
+// 1. Selecting the visual HTML pieces
+
+
+// TASK 1:
+/*
+    Set the accountBalance to any number you want (e.g., 1000) 
+    and set the correctPin to any 4-digit number (e.g., "1234")
+*/
+let accountBalance;
+let correctPin;
+
+// This keeps track of what the ATM screen is asking for ('PIN' or 'WITHDRAW')
+// let currentSystemStep = "PIN"; 
+
+// 3. THE TRIGGER: Runs every time the user clicks the SUBMIT button
+/*
+    Create a function that checks if the user pin has been entered correctly and switches
+    the system to the withdraw phase if it has been entered correctly. If not, display an error message.
+    If the user has entered the correct pin, check if the withdraw amount is less than or 
+    equal to the account balance. If it is, subtract the withdraw amount from the account balance 
+    and display a success message. If not, display an error message.
+*/
